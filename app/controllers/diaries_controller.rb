a887e73fafe49ada0441893181e2128a55425d0c
shuan <shuan@shuande-MacBook-Pro.local>
2017-02-06 20:42:32 +0800

    update

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,15 +1,28 @@
 class DiariesController < ApplicationController
 	before_action :authenticate_user!
-	before_action :set_diary, only: [:destroy]
+	before_action :set_diary, only: [:update, :destroy]
 
 	def index
 		@diaries = Diary.all
-		@diary = Diary.new
+		if params[:diary_id]
+			@diary = Diary.find(params[:diary_id])
+			if not current_user==@diary.user
+				flash[:alert] = "It's not your diary!"
+				redirect_to diaries_path
+			end
+		else
+			@diary = Diary.new
+		end
 	end
 
 	def create
 		@diary = current_user.diaries.new(params_permit)
 		@diary.save
+		redirect_to diaries_path
+	end
+
+	def update
+		@diary.update(params_permit)
 		redirect_to diaries_path
 	end
 
